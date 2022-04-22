28fd44550b0b5bd3589f6ec14025e75ebd0c4514
Fernando Ribeiro <flsr.dev@gmail.com>
2022-04-22 17:23:00 -0300

    feat(app.jsx): create favorite page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Album from './pages/Album';
+import Favorites from './pages/Favorites';
 import Login from './pages/Login';
 import Search from './pages/Search';
 
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/search" element={<Search />} />
       <Route path="/album/:id" element={<Album />} />
+      <Route path="/favorites" element={<Favorites />} />
     </Routes>
   );
 }
