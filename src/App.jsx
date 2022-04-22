aa3189b1841997df43455b600d434802de9d0b72
Fernando Ribeiro <flsr.dev@gmail.com>
2022-04-22 17:19:43 -0300

    feat(app.jsx): create search page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Search from './pages/Search';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/search" element={<Search />} />
     </Routes>
   );
 }
