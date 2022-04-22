fc59c1cd761100950b0186de0fec74ab4c498230
Fernando Ribeiro <flsr.dev@gmail.com>
2022-04-22 19:56:14 -0300

    feat(login.jsx): create function to check username length

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,10 +3,13 @@ import { UserContext } from '../context/contexts';
 
 export default function Login() {
   const [username, setTempUsername] = useState('');
+  const [isLoginDisabled, setIsLoginDisabled] = useState(true);
   const { setUserName } = useContext(UserContext);
 
   const checkUserLength = () => {
-
+    const MIN_USERNAME_LENGTH = 3;
+    if (username >= 3) setIsLoginDisabled(false);
+    if (username <= 3) setIsLoginDisabled(true);
   };
 
   const handleInputChange = ({ target: { value } }) => {
@@ -14,7 +17,13 @@ export default function Login() {
   };
   return (
     <div>
-      <input type="text" name="username" aria-label="user name" value={username} onChange={handleInputChange} />
+      <input
+        type="text"
+        name="username"
+        aria-label="user name"
+        value={username}
+        onChange={handleInputChange}
+      />
       <button type="button">Login</button>
 
     </div>
