d5e2baff406dd5722c975ebe935bd25f1b144129
Fernando Ribeiro <flsr.dev@gmail.com>
2022-04-22 17:21:36 -0300

    feat(app.jsx): create album route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Album from './pages/Album';
 import Login from './pages/Login';
 import Search from './pages/Search';
 
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/search" element={<Search />} />
+      <Route path="/album/:id" element={<Album />} />
     </Routes>
   );
 }
