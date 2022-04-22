98501b5151e2826fcf2fa74d41317303709e8c95
Fernando Ribeiro <flsr.dev@gmail.com>
2022-04-22 16:53:53 -0300

    test(app.js): create login page route test

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,3 +1,4 @@
+import { screen } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
@@ -5,5 +6,7 @@ import renderWithRouter from './helpers/renderWithRouter';
 describe('Test <App/> routes', () => {
   it('tests if the login page is rendered', () => {
     renderWithRouter(<App />);
+    const loginButton = screen.getAllByRole('button', { name: /login/i });
+    expect(loginButton).toBeInTheDocument();
   });
 });
