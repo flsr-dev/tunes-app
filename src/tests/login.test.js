bdb01cbe8e180704f6d6f9d84a84b1835703e082
Fernando Ribeiro <flsr.dev@gmail.com>
2022-04-23 12:56:58 -0300

    test((login.test.js)): test if login button is enabled only when passed a valid username length

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import Login from '../pages/Login';
 import renderWithRouter from './helpers/renderWithRouter';
 import UserProvider from '../context/UserProvider';
@@ -14,6 +15,24 @@ describe('Login Page:', () => {
   it('tests if the login button starts disabled', () => {
     renderWithRouter(<UserProvider><Login /></UserProvider>, ['/login']);
     const loginButton = screen.getByRole('button', { name: /login/i });
+    expect(loginButton).toBeDisabled();
+  });
+
+  it('tests if the button changes to enabled only when passed a valid username length', () => {
+    renderWithRouter(<UserProvider><Login /></UserProvider>, ['/login']);
+    const loginButton = screen.getByRole('button', { name: /login/i });
+    const nameInput = screen.getByLabelText(/user name/i);
+    const invalidUserLength = 'us';
+    const validUserLength = 'use';
+
+    userEvent.type(nameInput, invalidUserLength);
+    expect(loginButton).toBeDisabled();
+
+    userEvent.type(nameInput, `{selectall}{del}${validUserLength}`);
+    expect(loginButton).not.toBeDisabled();
+
+    nameInput.setSelectionRange(0, 2);
+    userEvent.type(nameInput, '{del}');
     expect(loginButton).toBeDisabled();
   });
 });
