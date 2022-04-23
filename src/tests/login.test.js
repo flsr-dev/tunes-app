6cde11b22301d225855e43c0167d8090499d5e7e
Fernando Ribeiro <flsr.dev@gmail.com>
2022-04-23 12:41:04 -0300

    test((login.test.js)): add test to check if login button starts disabled

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -2,11 +2,18 @@ import { screen } from '@testing-library/react';
 import React from 'react';
 import Login from '../pages/Login';
 import renderWithRouter from './helpers/renderWithRouter';
+import UserProvider from '../context/UserProvider';
 
 describe('Login Page:', () => {
   it('tests if the name input field is in the document', () => {
-    renderWithRouter(<Login />, ['/login']);
+    renderWithRouter(<UserProvider><Login /></UserProvider>, ['/login']);
     const nameInput = screen.getByLabelText(/user name/i);
     expect(nameInput).toBeInTheDocument();
+  });
+
+  it('tests if the login button starts disabled', () => {
+    renderWithRouter(<UserProvider><Login /></UserProvider>, ['/login']);
+    const loginButton = screen.getByRole('button', { name: /login/i });
+    expect(loginButton).toBeDisabled();
   });
 });
